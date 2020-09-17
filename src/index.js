40ef7034412894c66867fbf434d9a6d40eb7e3d6
keaton <keatonrgallagher@gmail.com>
2020-09-17 00:18:35 -0600

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
-import './index.css';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import ingredientsReducer from './store/reducers/ingredientsReducer'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+
+const rootReducer = combineReducers({
+    ingReducer: ingredientsReducer
+})
+
+const store = createStore(rootReducer)
 
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
 
-ReactDOM.render( app, document.getElementById( 'root' ) );
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
